9fc15250a77d49a8dd2734ee934fddaa8eae576b
alistairrutherford <alistair.rutherford@gmail.com>
2017-02-21 14:16:05 +0000

    Done.

--- bin/eurler9.dart
+++ bin/eurler9.dart
@@ -1,5 +1,14 @@
 /**
  *
+    Special Pythagorean triplet
+    Problem 9
+    A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+    a2 + b2 = c2
+    For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+    There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    Find the product abc.
  */
 
 import 'dart:math';
@@ -8,35 +17,32 @@ main(List<String> args) {
 
   const int TARGET = 1000;
 
+  bool found = false;
+
   int a = 1;
   int b = 2;
   int c = 3;
-
-  bool found = false;
-
   while (a < TARGET && !found) {
+    b = 2;
 
     while (b < TARGET && !found){
 
-      while (c < TARGET && !found) {
+      c = TARGET - a - b;
 
-        int value = a +  b  + c;
-
-        if (value == TARGET) {
-
-          if ((pow(a, 2) + pow(b, 2)) == pow(c, 2)) {
-            found = true;
-          }
-        }
-         c++;
+      if ((pow(a, 2) + pow(b, 2)) == pow(c, 2)) {
+        found = true;
       }
-      b++;
+      else
+        b++;
     }
-    a++;
+
+    if (!found) {
+      a++;
+    }
   }
 
   if (found) {
-    print('Answer, $a, $b, $c');
+    print('Answer, $a, $b, $c, Product =${ a * b * c}');
   } else {
     print('Not Found!');
   }
