140556d5f344bb3b726b8b78a09f3f73f3468661
alistairrutherford <alistair.rutherford@gmail.com>
2017-03-03 09:04:28 +0000

    wip

---
+++ bin/euler12.dart
@@ -0,0 +1,11 @@
+/**
+ * What is the value of the first triangle number to have over five hundred divisors?
+ *
+ */
+
+
+main(List<String> args) {
+  int count = 0;
+
+  print('Result is $count');
+}
