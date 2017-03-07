7660e2a2ba2e989d1350fada0ce334ef23259291
alistairrutherford <alistair.rutherford@gmail.com>
2017-03-07 08:32:54 +0000

    wip

--- bin/euler12.dart
+++ bin/euler12.dart
@@ -3,9 +3,34 @@
  *
  */
 
+import 'dart:math';
 
 main(List<String> args) {
+  const int FACTOR_TARGET = 500;
+
   int count = 0;
+
+  bool found = false;
+
+  int index = 1;
+  while (!found) {
+    int value = (index * (index + 1)) ~/ 2;
+
+    int factorCount = 1;
+
+    int limit = sqrt(value).toInt();
+
+    for (int factor = index; factor < limit; factor++) {
+
+      if (factor % factorCount == 0) {
+        factorCount++;
+      }
+    }
+
+    if (factorCount >= FACTOR_TARGET) {
+
+    }
+  }
 
   print('Result is $count');
 }
